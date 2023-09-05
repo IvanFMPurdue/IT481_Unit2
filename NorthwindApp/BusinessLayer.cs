41e4d4b07d84dc4a24b76c28a1e1bb2d4fc48db0
Ivan F Martinovich <143536214+IvanFMPurdue@users.noreply.github.com>
2023-09-05 18:00:31 -0500

    Add files via upload

---
+++ NorthwindApp/BusinessLayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace NorthwindApp
+{
+    class BusinessLayer
+    {
+        private DataAccessLayer _dal;
+
+        public BusinessLayer(string connection)
+        {
+            _dal = new DataAccessLayer(connection);
+        }
+
+        public int GetNumberOfCustomers()
+        {
+            return _dal.GetNumberOfCustomers();
+        }
+
+        public List<string> GetCustomerLastName()
+        {
+            return _dal.GetCustomerLastName();
+        }
+    }
+}
---
+++ NorthwindApp/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace NorthwindApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private BusinessLayer _bl = new BusinessLayer(@"Data Source=IVANPC\IT350_2301C;Initial Catalog=Northwind;Integrated Security=True");
+
+        private void LoadDataBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NumberOfCustomersTbx.Text = $"{_bl.GetNumberOfCustomers()}";
+            CustomerNamesLbx.ItemsSource = _bl.GetCustomerLastName();
+        }
+    }
+}
